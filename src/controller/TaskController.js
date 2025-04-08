4323adfe4f09df1cd8ed699614a551f5b693fcdb
rodrigoreis99 <rodrigo.reis.prado@gmail.com>
2025-04-08 18:53:06 -0300

    Desenvolvido funções para filtrar todas as tarefas; Filtrar tarefa por ID; Deletar tarefas.

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -23,6 +23,40 @@ class TaskController{
         return res.status(500).json(error);
       })
     }
+
+    async all(req, res){
+        await TaskModel.find({macaddress: {'$in':req.body.macaddress}})
+                .sort('when')
+                .then(response => {
+                    return res.status(200).json(response);
+                })
+                .catch(error =>{
+                    return res.status(500).json(error);
+                });
+    }
+
+    async show(req,res){
+        await TaskModel.findById(req.params.id)
+        .then(response => {
+            if(response)
+                return res.status(200).json(response);
+            else
+                return res.status(404).json({error: 'Tarefa não encontrada!'});
+        })
+        .catch(error =>{
+            return res.status(500).json(error);
+        })
+    }
+
+    async delete(req,res){
+        await TaskModel.deleteOne({'_id': req.params.id})
+                .then(response =>{
+                    return res.status(200).json(response)
+                })
+                .catch(response =>{
+                    return res.status(500).json(error);
+                })
+    }
 }
 
 module.exports = new TaskController();
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router(); // importa o router
 
 const TaskController = require('../controller/TaskController'); //importa o task controller
 const TaskValidation = require('../middlewares/TaskValidation');
+const MacAddressValidation = require ('../middlewares/MacAddressValidation');
 
 router.post('/', TaskValidation,TaskController.create);
 router.put('/:id', TaskValidation,TaskController.update);
-
+router.get('/filter/all', MacAddressValidation,TaskController.all);
+router.get('/:id',TaskController.show);
+router.delete('/:id', TaskController.delete);
 
 module.exports = router;
