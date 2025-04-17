cea3294b584c26f6a825f2f0b1d2a2e2ec147d76
rodrigoreis99 <rodrigo.reis.prado@gmail.com>
2025-04-17 09:21:28 -0300

    Configurado navegação de paginas entre home e nova tarefa

--- Web/web/src/components/header/index.js
+++ Web/web/src/components/header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as S from './styles';
 import logo from '../../assets/logo.png';
 import bell from '../../assets/bell.png';
+import {Link} from 'react-router-dom';
 
 
 function Header({lateCount,clickNotification}) {
@@ -12,9 +13,9 @@ function Header({lateCount,clickNotification}) {
       </S.LeftSide>
 
       <S.RightSide >
-      <a href='#'>INÍCIO</a>
+      <Link to="/">INÍCIO</Link>
       <span className='dividir'/>
-      <a href='#'>NOVA TAREFA</a>
+      <Link to= "/task">NOVA TAREFA</Link>
       <span className='dividir'/>
       <a href='#'>SINCRONIZAR CELULAR</a>
       <span className='dividir'/>
